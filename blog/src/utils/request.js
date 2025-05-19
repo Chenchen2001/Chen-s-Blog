dd97d5541ec2b6ff5f5f5624565fedec3bbc3f10
PeterChen01 <77436871+Chenchen2001@users.noreply.github.com>
2025-05-19 20:58:41 +0800

    Update request.js

--- blog/src/utils/request.js
+++ blog/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-  baseURL: 'https://blog.chenchen.ip-ddns.com', // 后端地址
+  baseURL: '<Your Request Backend URL here>', 
 });
 
 request.interceptors.request.use((config) => {
