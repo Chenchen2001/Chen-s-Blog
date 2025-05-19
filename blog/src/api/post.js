2cc6a95f96de234655d6b85c0b2d679f602e0edf
PeterChen01 <77436871+Chenchen2001@users.noreply.github.com>
2025-05-19 22:23:26 +0800

    Update post.js

--- blog/src/api/post.js
+++ blog/src/api/post.js
@@ -1,6 +1,6 @@
 import request from '../utils/request';
 
-export const getPosts = () => request.get('/posts');
+export const getPosts = (params = {}) => request.get('/posts', { params });
 export const getPostById = (id) => request.get(`/posts/${id}`);
 export const createPost = (data) => request.post('/posts', data);
 export const updatePost = (id, data) => request.put(`/posts/${id}`, data);
